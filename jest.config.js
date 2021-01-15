c7731a8886f178b2e3adc21119953412e39b0f0b
fullstackfool <k.a.davies@hotmail.co.uk>
2021-01-15 14:47:56 +0000

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    rootDir: './',
+    transform: {
+        "^.+\\.[t]sx?$": "ts-jest"
+    },
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/index.ts'
+    ],
+    cacheDirectory: "<rootDir>/tests/cache",
+    coverageDirectory: "<rootDir>/tests/coverage",
+    coverageProvider: "babel",
+    coverageReporters: ["json", "text-summary"],
+    testMatch: [
+        "<rootDir>tests/**/*(*.)@(test).[tj]s?(x)"
+    ],
+    automock: false,
+    unmockedModulePathPatterns: [
+        "<rootDir>/node_modules/"
+    ],
+    errorOnDeprecated: true,
+    testPathIgnorePatterns: [
+        '/node_modules/'
+    ],
+    bail: true,
+    notify: true,
+    notifyMode: 'failure-change',
+    globals: {
+        window: {}
+    },
+    extraGlobals: [
+        "Function",
+        "Array",
+        "String",
+        "Math"
+    ]
+};
